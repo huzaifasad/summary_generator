7081c16fc6f5b61d341658ec3b07c8649951a451
Huzaifa Saad <140094374+huzaifasad@users.noreply.github.com>
2024-04-26 09:49:18 -0700

    Update route.ts

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-  apiKey: 'sk-proj-juOApKucZsBgOfO5jNz3T3BlbkFJpeJTgYlasjLDT4whmvAp', // Replace with your actual API key
+  apiKey:process.env.OPENAI_API_KEY, // Replace with your actual API key
 });
 
 export async function POST(req: Request, res: NextResponse) {
